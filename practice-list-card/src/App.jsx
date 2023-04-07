791529bf9b0f0fcd8f39b6d26c3607aff6921523
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-07 11:25:44 +0600

    Navbar added

--- practice-list-card/src/App.jsx
+++ practice-list-card/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './assets/components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-7xl text-emerald-500'>React Tailwind</h1>
-
+      <Navbar></Navbar>
     </div>
   )
 }
---
+++ practice-list-card/src/assets/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import { list } from 'postcss';
+import React from 'react';
+
+const Navbar = () => {
+    const routes = [
+        {
+            id: 1,
+            name: 'Home',
+            path: '/'
+        },
+        {
+            id: 2,
+            name: 'About',
+            path: '/about'
+        },
+        {
+            id: 3,
+            name: 'Contact',
+            path: '/contact'
+        },
+        {
+            id: 4,
+            name: 'Products',
+            path: '/products'
+        },
+        {
+            id: 5,
+            name: 'Services',
+            path: '/services'
+        }
+    ];
+
+    return (
+        <div>
+            {
+                routes.map(route => <li> {route.name} </li>)
+            }
+
+        </div>
+    );
+};
+
+export default Navbar;
