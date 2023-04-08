b0b37f6e2ff5cd0decc3f222d1632216671f50e6
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-08 10:59:17 +0600

    multiple component added

--- introduction-with-react-router/src/main.jsx
+++ introduction-with-react-router/src/main.jsx
@@ -6,29 +6,63 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import About from './assets/components/About/About';
+import Blog from './assets/components/Blog/Blog';
+import Contact from './assets/components/Contact/Contact';
+import ErrorPage from './assets/components/Error/ErrorPage';
+import Header from './assets/components/Header/Header';
+import Home from './assets/components/Home/Home';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   },
+//   {
+//     path: "/about",
+//     element: <About></About>,
+//   },
+//   {
+//     path: "/blog",
+//     element: <Blog></Blog>
+//   },
+//   {
+//     path: "/contact",
+//     element: <Contact></Contact>,
+//   },
+//   {
+
+//   }
+
+// ]);
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
-  },
-  {
-    path: "/about",
-    element: <div>This is about page</div>,
-  },
-  {
-    path: "/blog",
-    element: <div>This is blog component</div>,
-  },
-  {
-    path: "/contact",
-    element: <div>Contact us right now</div>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'blog',
+        element: <Blog></Blog>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
+
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
+    <Header></Header>
     {/* <App /> */}
   </React.StrictMode>,
 )
