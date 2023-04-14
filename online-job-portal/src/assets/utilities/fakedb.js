42183c734253d346e1442a669ac3765c848eff5a
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-14 23:04:24 +0600

    Add to DB

--- online-job-portal/src/assets/utilities/fakedb.js
+++ online-job-portal/src/assets/utilities/fakedb.js
@@ -1,44 +1,30 @@
-// use local storage to manage cart data
-const addToDb = id => {
-    let shoppingCart = getShoppingCart();
-    // add quantity
-    const quantity = shoppingCart[id];
-    if (!quantity) {
-        shoppingCart[id] = 1;
+const addToDb = (details) => {
+
+    const storedData = localStorage.getItem('jobDetails');
+
+    if (storedData) {
+        const existingData = JSON.parse(storedData);
+        localStorage.setItem('jobDetails', JSON.stringify([...existingData, details]));
+    } else {
+        localStorage.setItem('jobDetails', JSON.stringify([details]));
     }
-    else {
-        const newQuantity = quantity + 1;
-        shoppingCart[id] = newQuantity;
+
+}
+
+const getFromDb = () => {
+
+    let data = []
+
+    const storedData = localStorage.getItem('jobDetails');
+    if (storedData) {
+        data = JSON.parse(storedData);
+        console.log(data);
+    } else {
+        console.log('No data found');
     }
-    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+
+    return data
+
 }
 
-const removeFromDb = id => {
-    const shoppingCart = getShoppingCart();
-    if (id in shoppingCart) {
-        delete shoppingCart[id];
-        localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-    }
-}
-
-const getShoppingCart = () => {
-    let shoppingCart = {};
-
-    //get the shopping cart from local storage
-    const storedCart = localStorage.getItem('shopping-cart');
-    if (storedCart) {
-        shoppingCart = JSON.parse(storedCart);
-    }
-    return shoppingCart;
-}
-
-const deleteShoppingCart = () => {
-    localStorage.removeItem('shopping-cart');
-}
-
-export {
-    addToDb,
-    removeFromDb,
-    getShoppingCart,
-    deleteShoppingCart
-}
+export { addToDb, getFromDb }
