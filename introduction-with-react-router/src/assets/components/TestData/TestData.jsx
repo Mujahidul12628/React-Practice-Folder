09f6173aa14c9cf19b8fdc778b9343e64075151b
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-19 23:36:02 +0600

    test content

---
+++ introduction-with-react-router/src/assets/components/TestData/TestData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestData = () => {
+    return (
+        <div>
+            <h3>This the test data</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse fugit consequuntur nesciunt? Cupiditate itaque atque quia repellendus tenetur sint?</p>
+        </div>
+    );
+};
+
+export default TestData;
