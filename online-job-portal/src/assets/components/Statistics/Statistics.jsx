72b187e296e34b6809881d97c6f5b8dad3ebbb2c
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-09 15:05:16 +0600

    Statistics of applying job

--- online-job-portal/src/assets/components/Statistics/Statistics.jsx
+++ online-job-portal/src/assets/components/Statistics/Statistics.jsx
@@ -1,3 +1,89 @@
+// import React from 'react';
+// import {
+//     ComposedChart,
+//     Line,
+//     Bar,
+//     XAxis,
+//     YAxis,
+//     CartesianGrid,
+//     Tooltip,
+//     Legend
+// } from "recharts";
+
+// const data = [
+//     {
+//         name: "A1",
+//         marks: 60,
+
+
+//     },
+//     {
+//         name: "A2",
+//         marks: 57,
+
+
+//     },
+//     {
+//         name: "A3",
+//         marks: 60,
+//     },
+//     {
+//         name: "A4",
+//         marks: 58,
+//     },
+//     {
+//         name: "A5",
+//         marks: 40,
+//     },
+//     {
+//         name: "A6",
+//         marks: 60,
+//     },
+//     {
+//         name: "A7",
+//         marks: 50,
+//     },
+//     {
+//         name: "A8",
+//         marks: 48,
+//     },
+
+
+// ];
+
+// const Statistics = () => {
+//     return (
+//         <div className='row'>
+//             <div className='py-5 my-3' id='details-background'>
+//                 <h2 className='py-5 text-center'>Statistics component</h2>
+//             </div>
+//             <div className='d-flex justify-content-center align-items-center col-12' id='chart'>
+//                 <ComposedChart
+//                     width={800}
+//                     height={500}
+//                     data={data}
+//                     margin={{
+//                         top: 10,
+//                         right: 10,
+//                         bottom: 10,
+//                         left: 10
+//                     }}
+//                 >
+//                     <CartesianGrid stroke="#f5f5f5" />
+//                     <XAxis dataKey="name" scale="band" />
+//                     <YAxis />
+//                     <Tooltip />
+//                     <Legend />
+//                     <Bar dataKey="marks" barSize={20} fill="#5187" />
+//                     <Line type="monotone" dataKey="marks" stroke="#ff7300" />
+//                 </ComposedChart>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Statistics;
+
 import React from 'react';
 import {
     ComposedChart,
@@ -12,43 +98,29 @@ import {
 
 const data = [
     {
-        name: "A1",
-        marks: 60,
-
-
+        name: "Web Developer",
+        totalApply: 5590,
     },
     {
-        name: "A2",
-        marks: 57,
-
-
+        name: "Software Engineer",
+        totalApply: 4530,
     },
     {
-        name: "A3",
-        marks: 60,
+        name: "Blockchain Developer",
+        totalApply: 500,
     },
     {
-        name: "A4",
-        marks: 58,
+        name: "ML Engineer",
+        totalApply: 1790,
     },
     {
-        name: "A5",
-        marks: 40,
+        name: "Game Development",
+        totalApply: 4980,
     },
     {
-        name: "A6",
-        marks: 60,
-    },
-    {
-        name: "A7",
-        marks: 50,
-    },
-    {
-        name: "A8",
-        marks: 48,
-    },
-
-
+        name: "UI/UX Engineer",
+        totalApply: 3586,
+    }
 ];
 
 const Statistics = () => {
@@ -74,8 +146,8 @@ const Statistics = () => {
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    <Bar dataKey="marks" barSize={20} fill="#5187" />
-                    <Line type="monotone" dataKey="marks" stroke="#ff7300" />
+                    <Bar dataKey="totalApply" barSize={20} fill="#5187" />
+                    <Line type="monotone" dataKey="totalApply" stroke="#ff7300" />
                 </ComposedChart>
             </div>
         </div>
